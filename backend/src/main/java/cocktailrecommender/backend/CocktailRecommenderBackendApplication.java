bd49b2d475a1f7887c2a143539907b96aedcdd6f
noir-01 <smu08054@gmail.com>
2024-07-30 19:18:24 +0900

    Build: Initialize React Native, Spring Project

---
+++ backend/src/main/java/cocktailrecommender/backend/CocktailRecommenderBackendApplication.java
@@ -0,0 +1,13 @@
+package cocktailrecommender.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocktailRecommenderBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocktailRecommenderBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/cocktailrecommender/backend/CocktailRecommenderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cocktailrecommender.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocktailRecommenderBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
