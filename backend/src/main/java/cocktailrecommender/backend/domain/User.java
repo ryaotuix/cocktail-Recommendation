36dee5d550485d09420c7be73c927da15836358b
noir-01 <smu08054@gmail.com>
2024-08-04 00:40:59 +0900

    Fix: User Entity - Add email field & getter/setter/contructor

--- backend/src/main/java/cocktailrecommender/backend/domain/User.java
+++ backend/src/main/java/cocktailrecommender/backend/domain/User.java
@@ -9,12 +9,52 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
-
+    private String email;
     private String password;
     private String name;
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 
     @OneToMany(mappedBy = "user")
     private Set<UserIngredient> userIngredients;
 
     // getters and setters
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUserIngredients(Set<UserIngredient> userIngredients) {
+        this.userIngredients = userIngredients;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Set<UserIngredient> getUserIngredients() {
+        return userIngredients;
+    }
 }
