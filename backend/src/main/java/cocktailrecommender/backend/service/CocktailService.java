6730690b777ddc873251fdee75d9048b0141c18b
noir-01 <smu08054@gmail.com>
2024-08-05 01:11:44 +0900

    Feat: Create CocktailService
    cocktail CRUD

---
+++ backend/src/main/java/cocktailrecommender/backend/service/CocktailService.java
@@ -0,0 +1,63 @@
+package cocktailrecommender.backend.service;
+
+import cocktailrecommender.backend.DTO.CocktailDTO;
+import cocktailrecommender.backend.domain.Cocktail;
+import cocktailrecommender.backend.repository.CocktailRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class CocktailService {
+    @Autowired
+    private final CocktailRepository cocktailRepository;
+
+    public CocktailService(CocktailRepository cocktailRepository) {
+        this.cocktailRepository = cocktailRepository;
+    }
+
+    public boolean createCocktail(CocktailDTO.CocktailDTOWithoutId cocktailDTOWithoutId){
+        //if same name exists: cannot create
+        if(cocktailRepository.findByName(cocktailDTOWithoutId.getName()).isPresent()){
+            return false;
+        }else{
+            Cocktail cocktail = new Cocktail();
+            cocktail.setName(cocktailDTOWithoutId.getName());
+            cocktail.setHowToMake(cocktailDTOWithoutId.getHowToMake());
+            cocktailRepository.save(cocktail);
+            return true;
+        }
+    }
+
+    //find cocktails by name
+    public List<CocktailDTO.CocktailDTOWithId> findCocktailsByName(String name){
+        return cocktailRepository.findByName(name).stream()
+                .map(CocktailDTO.CocktailDTOWithId::from)
+                .collect(Collectors.toList());
+    }
+
+    //Edit howToMake
+    public boolean editHowToMake(Long cocktailId, String newHowToMake){
+        return cocktailRepository.findById(cocktailId)
+                .map(cocktail -> {
+                    cocktail.setHowToMake(newHowToMake);
+                    cocktailRepository.save(cocktail);
+                    return true;
+                })
+                .orElse(false);
+    }
+
+    //Delete Cocktail
+    public boolean deleteCocktail(Long cocktailId){
+        Optional<Cocktail> cocktail = cocktailRepository.findById(cocktailId);
+        if (cocktail.isPresent()) {
+            cocktailRepository.deleteById(cocktailId);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
