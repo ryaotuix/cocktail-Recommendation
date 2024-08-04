3f89867b27fc5024012bb411d5d2e20743a8b62e
noir-01 <smu08054@gmail.com>
2024-08-05 01:10:08 +0900

    Fix: Change DTO structure

--- backend/src/main/java/cocktailrecommender/backend/DTO/CocktailDTO.java
+++ backend/src/main/java/cocktailrecommender/backend/DTO/CocktailDTO.java
@@ -1,65 +1,34 @@
 package cocktailrecommender.backend.DTO;
 
+import cocktailrecommender.backend.domain.Cocktail;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.Set;
 
 public class CocktailDTO {
-
-    private Long cocktailId;
-    private String name;
-    private String howToMake;
-    private Set<CocktailIngredientDTO> cocktailIngredients;
-    private Set<CocktailTasteDTO> cocktailTastes;
-
-    // Constructors
-    public CocktailDTO() {
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class CocktailDTOWithoutId{
+        private String name;
+        private String howToMake;
     }
-
-    public CocktailDTO(Long cocktailId, String name, String howToMake, Set<CocktailIngredientDTO> cocktailIngredients, Set<CocktailTasteDTO> cocktailTastes) {
-        this.cocktailId = cocktailId;
-        this.name = name;
-        this.howToMake = howToMake;
-        this.cocktailIngredients = cocktailIngredients;
-        this.cocktailTastes = cocktailTastes;
-    }
-
-    // Getters and setters
-    public Long getCocktailId() {
-        return cocktailId;
-    }
-
-    public void setCocktailId(Long cocktailId) {
-        this.cocktailId = cocktailId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getHowToMake() {
-        return howToMake;
-    }
-
-    public void setHowToMake(String howToMake) {
-        this.howToMake = howToMake;
-    }
-
-    public Set<CocktailIngredientDTO> getCocktailIngredients() {
-        return cocktailIngredients;
-    }
-
-    public void setCocktailIngredients(Set<CocktailIngredientDTO> cocktailIngredients) {
-        this.cocktailIngredients = cocktailIngredients;
-    }
-
-    public Set<CocktailTasteDTO> getCocktailTastes() {
-        return cocktailTastes;
-    }
-
-    public void setCocktailTastes(Set<CocktailTasteDTO> cocktailTastes) {
-        this.cocktailTastes = cocktailTastes;
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class CocktailDTOWithId{
+        private Long cocktailId;
+        private String name;
+        private String howToMake;
+        public static CocktailDTOWithId from(Cocktail cocktail){
+            return new CocktailDTOWithId(
+                    cocktail.getCocktailId(),cocktail.getName(), cocktail.getHowToMake()
+            );
+        }
     }
 }
