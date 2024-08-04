5112307e7b286602bc55a7b7f7f93fa800fb35f4
noir-01 <smu08054@gmail.com>
2024-08-04 19:19:10 +0900

    Fix: Add Lombok

--- backend/src/main/java/cocktailrecommender/backend/domain/User.java
+++ backend/src/main/java/cocktailrecommender/backend/domain/User.java
@@ -1,10 +1,16 @@
 package cocktailrecommender.backend.domain;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Set;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class User {
 
     @Id
@@ -20,44 +26,4 @@ public class User {
 
     @OneToMany(mappedBy = "user")
     private Set<UserTaste> userTastes;
-
-    // getters and setters
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setUserIngredients(Set<UserIngredient> userIngredients) {
-        this.userIngredients = userIngredients;
-    }
-
-    public String getEmail() {return email;}
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Set<UserIngredient> getUserIngredients() {
-        return userIngredients;
-    }
 }
