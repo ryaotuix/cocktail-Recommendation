e00337ccc20d8b30e7ad196c75294109a62448de
noir-01 <smu08054@gmail.com>
2024-08-04 18:48:51 +0900

    Feat: Add UserController - register, login

---
+++ backend/src/main/java/cocktailrecommender/backend/controller/UserController.java
@@ -0,0 +1,49 @@
+package cocktailrecommender.backend.controller;
+
+import cocktailrecommender.backend.DTO.UserDTO;
+import cocktailrecommender.backend.service.UserService;
+import cocktailrecommender.backend.utils.JwtCertificate;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
+public class UserController {
+    UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> signUp(@RequestBody UserDTO userDTO){
+
+        if(userService.createUser(userDTO)){
+            return ResponseEntity.ok("Register Succeeded");
+        }
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Register Failed");
+    }
+    @GetMapping("/login")
+    public ResponseEntity<String> login(@RequestBody UserDTO userDTO){
+        JSONObject responseJson = new JSONObject();
+
+        //when login success
+        if(userService.login(userDTO)){
+            JwtCertificate jwtCertificate = new JwtCertificate();
+            String token = jwtCertificate.generateToken(userDTO.getEmail());
+            responseJson.put("token",token);
+            return ResponseEntity.ok(responseJson.toString());
+        }else{
+            responseJson.put("message", "login failed");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(responseJson.toString());
+        }
+    }
+}
