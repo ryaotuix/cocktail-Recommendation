9742a66fb5af27043dd35ddb355581ec42e0ade4
blanc <blanc@DESKTOP-23KS9M5>
2024-08-08 21:51:42 +0900

    Feat: CocktailIngredient Entity, DTO

--- backend/src/main/java/cocktailrecommender/backend/domain/CocktailIngredient.java
+++ backend/src/main/java/cocktailrecommender/backend/domain/CocktailIngredient.java
@@ -1,7 +1,11 @@
 package cocktailrecommender.backend.domain;
 import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Setter
+@NoArgsConstructor
 public class CocktailIngredient {
 
     @Id
