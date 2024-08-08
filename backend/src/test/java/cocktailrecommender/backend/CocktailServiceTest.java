66b1ad5daa4e96a77bb76506fd329ea6ca116a94
blanc <blanc@DESKTOP-23KS9M5>
2024-08-08 21:48:32 +0900

    Test: CocktailServiceTest
    create, delete, findCocktails, findCocktail

---
+++ backend/src/test/java/cocktailrecommender/backend/CocktailServiceTest.java
@@ -0,0 +1,56 @@
+package cocktailrecommender.backend;
+
+import cocktailrecommender.backend.DTO.CocktailDTO;
+import cocktailrecommender.backend.DTO.CocktailIngredientDTO;
+import cocktailrecommender.backend.domain.Ingredient;
+import cocktailrecommender.backend.repository.CocktailRepository;
+import cocktailrecommender.backend.repository.IngredientRepository;
+import cocktailrecommender.backend.repository.UserRepository;
+import cocktailrecommender.backend.service.CocktailService;
+import cocktailrecommender.backend.service.IngredientService;
+import cocktailrecommender.backend.service.UserService;
+import org.aspectj.lang.annotation.After;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class CocktailServiceTest {
+    @Autowired
+    private CocktailService cocktailService;
+    @Autowired
+    private CocktailRepository cocktailRepository;
+
+    @AfterEach
+    void clear(){
+        cocktailRepository.deleteAll();
+    }
+    @Test
+    void createCocktail(){
+        assertTrue(cocktailService.createCocktail(new CocktailDTO.CocktailDTOWithoutId("cocktail1","howToMake")));
+        assertFalse(cocktailService.createCocktail(new CocktailDTO.CocktailDTOWithoutId("cocktail1","howToMake")));
+        assertTrue(cocktailService.createCocktail(new CocktailDTO.CocktailDTOWithoutId("cocktail2","howToMake")));
+    }
+    @Test
+    void findCocktails(){
+        cocktailService.createCocktail(new CocktailDTO.CocktailDTOWithoutId("cocktail1","howToMake"));
+        cocktailService.createCocktail(new CocktailDTO.CocktailDTOWithoutId("cocktail2","howToMake"));
+        int idx=1;
+        for(CocktailDTO.CocktailDTOWithId dto: cocktailService.findCocktailsByName("cocktail")){
+            assertEquals("cocktail"+ idx++,dto.getName());
+        }
+        assertEquals("cocktail1",cocktailService.findCocktailByName("cocktail1").get().getName());
+    }
+
+    @Test
+    void deleteCocktail(){
+        cocktailService.createCocktail(new CocktailDTO.CocktailDTOWithoutId("cocktail1","howToMake"));
+        cocktailService.createCocktail(new CocktailDTO.CocktailDTOWithoutId("cocktail2","howToMake"));
+        assertTrue(cocktailService.deleteCocktail(cocktailService.findCocktailByName("cocktail1").get().getCocktailId()));
+        assertEquals("cocktail2",cocktailRepository.findAll().get(0).getName());
+    }
+}
