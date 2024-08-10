9541ce81f366d13dd4a77629cf171f3233e0ac49
noir-01 <smu08054@gmail.com>
2024-08-10 23:51:08 +0900

    Feat: Lombok 추가

--- backend/src/main/java/cocktailrecommender/backend/domain/UserCocktail.java
+++ backend/src/main/java/cocktailrecommender/backend/domain/UserCocktail.java
@@ -1,8 +1,16 @@
 package cocktailrecommender.backend.domain;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserCocktail {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
