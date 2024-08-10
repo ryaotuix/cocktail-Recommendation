85310647fd7eec64b51791ef061276b0299ab46b
noir-01 <smu08054@gmail.com>
2024-08-11 00:05:48 +0900

    Feat: UCRepo.findCocktailsByUserId

--- backend/src/main/java/cocktailrecommender/backend/repository/UserCocktailRepository.java
+++ backend/src/main/java/cocktailrecommender/backend/repository/UserCocktailRepository.java
@@ -1,8 +1,15 @@
 package cocktailrecommender.backend.repository;
 
+import cocktailrecommender.backend.domain.Cocktail;
 import cocktailrecommender.backend.domain.UserCocktail;
 import cocktailrecommender.backend.domain.UserIngredient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UserCocktailRepository extends JpaRepository<UserCocktail, Long> {
+    @Query("SELECT uc.cocktail FROM UserCocktail uc WHERE uc.user.userId = :userId")
+    List<Cocktail> findCocktailsByUserId(@Param("userId") Long userId);
 }
